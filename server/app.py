ef2df564675064fa7d9986e7269976bbfb1e25c2
Noah <noah_822@163.com>
2022-12-10 18:53:26 +0800

    update db API & server/pics

--- server/app.py
+++ server/app.py
@@ -1,3 +1,18 @@
-import os
-os.system('mysql -u root -p')
-os.system('root')
+from database.API import handler_pool
+
+from flask import Flask, render_template, request
+from flask import redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    for handler_thread in handler_pool.values():
+        handler_thread.start()
+    
+    app.run()
---
+++ server/database/API.py
@@ -0,0 +1,166 @@
+from threading import Thread
+from threading import Condition, Lock
+
+import mysql.connector
+
+class buffer:
+    def __init__(self):
+        self.queue = []
+    def is_empty(self):
+        return len(self.queue) == 0
+    def __len__(self):
+        return len(self.queue)
+    def pop(self):
+        item = self.queue[0]
+        self.queue = self.queue[1:]
+        return item
+    def push(self, item):
+        self.queue.append(item)
+    def len(self):
+        return len(self.queue)
+
+class query:
+    def __init__(self, script, res_buffer, interrupt):
+        self.script = script
+        self.res_buffer = res_buffer
+        self.interrupt = interrupt
+        
+        if script.strip().startswith('SELECT'):
+            self.op_type = 'retrieve'
+        else:
+            self.op_type = 'update'
+        
+
+
+USER_MODE = ['consumer', 'saler', 'super']
+
+LOG_FILE = './database.log'
+
+interrupt_vector = {
+    mode : Condition() for mode in USER_MODE
+}
+
+job_queue = {
+    mode : buffer() for mode in USER_MODE
+}
+
+job_queue_mutex = {
+    mode : Lock() for mode in USER_MODE
+}
+
+
+def run(db, query_to_serve):
+    cursor = db.cursor()
+    wait_cond = query_to_serve.interrupt
+    cursor.execute(query_to_serve.script)
+    
+    wait_cond.acquire()
+    
+    if query_to_serve.op_type == 'retrieve':
+        query_to_serve.res_buffer.append(cursor.fetchall())
+    else:
+        try:
+            db.commit()
+        except:
+            db.rollback()
+            
+    wait_cond.notify()
+    wait_cond.release()
+    
+    
+
+
+def record():
+    with open(LOG_FILE, 'w') as log_file:
+        pass
+
+def handler_init(mode):
+    def handler():
+        wait_cond = interrupt_vector[mode]
+        handler_buffer = job_queue[mode]
+        handler_mutex = job_queue_mutex[mode]
+
+        db = mysql.connector.connect(
+            host = 'localhost',
+            user = mode,
+            port = 3306,
+            database = 'AGDP'
+        )
+        
+        
+        while True:
+            wait_cond.acquire()
+            while len(handler_buffer) == 0:
+                wait_cond.wait()
+            handler_mutex.acquire()
+            query_to_serve = handler_buffer.pop()
+            handler_mutex.release()
+            
+            run(db, query_to_serve)
+            wait_cond.release()
+                
+    return Thread(target=handler)
+
+handler_pool = {
+    mode : handler_init(mode) for mode in USER_MODE
+}
+
+def handler(mode):
+    assert mode in USER_MODE, 'wrong user mode identifier'
+    
+    wait_cond = interrupt_vector[mode]
+    handler_buffer = job_queue[mode]
+    handler_mutex = job_queue_mutex[mode]
+    
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            script = func(*args, **kwargs)
+            res_buffer = []
+            __query = query(script, res_buffer, Condition())
+            
+            
+            wait_cond.acquire()
+            handler_mutex.acquire()
+            if len(handler_buffer) == 0:
+                wait_cond.notify()
+            
+            handler_buffer.push(__query)
+            handler_mutex.release()
+            wait_cond.release()
+            
+            __query.interrupt.acquire()
+            while len(__query.res_buffer) == 0:
+                __query.interrupt.wait()
+            __query.interrupt.release()
+            
+            return __query.res_buffer[0]
+                     
+        return wrapper
+    
+    return decorator
+
+
+
+
+
+
+@handler('super')
+def game_type_insert():
+    # script = (
+    #     "INSERT INTO prefer (`con_id`, `cate_id`)"
+    #     "VALUES ("
+    #     "'10',"
+    #     "'20')"
+    # )
+    script = (
+        "SELECT * from publisher"
+    )
+    return script
+
+
+
+
+
+
+
+
